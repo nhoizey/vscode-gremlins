6318af35a0a950a3bc44889de3a08657615c80c2
Muah <ctf0@users.noreply.github.com>
2019-05-25 16:47:45 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,164 +1,139 @@
 var vscode = require('vscode')
 
-const gremlinsConfig = {
-  '00a0': {
-    description: 'non breaking space',
-  },
-  '200b': {
-    zeroWidth: true,
-    description: 'zero width space',
-  },
-  '200c': {
-    zeroWidth: true,
-    description: 'zero width non-joiner',
-  },
-  '201c': {
-    description: 'left double quotation mark',
-    backgroundColor: 'rgba(255,127,80,.5)',
-    overviewRulerColor: 'rgba(255,127,80,1)',
-  },
-  '201d': {
-    description: 'right double quotation mark',
-    backgroundColor: 'rgba(255,127,80,.5)',
-    overviewRulerColor: 'rgba(255,127,80,1)',
-  },
-  fffc: {
-    zeroWidth: true,
-    description: 'object replacement character',
-  },
-}
+const gremlinsConfig = vscode.workspace.getConfiguration('gremlins').characters
+const gutterIconSize = vscode.workspace.getConfiguration('gremlins').gutterIconSize
 
 function gremlinsFromConfig(context) {
-  const lightIcon = {
-    gutterIconPath: context.asAbsolutePath('images/gremlins-light.svg'),
-    gutterIconSize: '75%',
-  }
-  const darkIcon = {
-    gutterIconPath: context.asAbsolutePath('images/gremlins-dark.svg'),
-    gutterIconSize: '75%',
-  }
-
-  const gremlins = {}
-  for (const [hexCodePoint, config] of Object.entries(gremlinsConfig)) {
-    let decorationType = {
-      light: lightIcon,
-      dark: darkIcon,
-      overviewRulerColor: config.overviewRulerColor || 'darkred',
-      overviewRulerLane: vscode.OverviewRulerLane.Right,
+    const lightIcon = {
+        gutterIconPath: context.asAbsolutePath('images/gremlins-light.svg'),
+        gutterIconSize: gutterIconSize
+    }
+    const darkIcon = {
+        gutterIconPath: context.asAbsolutePath('images/gremlins-dark.svg'),
+        gutterIconSize: gutterIconSize
     }
 
-    if (config.zeroWidth) {
-      decorationType.borderWidth = '1px'
-      decorationType.borderStyle = 'solid'
-      decorationType.borderColor = config.borderColor || 'darkred'
-    } else {
-      decorationType.backgroundColor =
-        config.backgroundColor || 'rgba(255,128,128,.5)'
+    const gremlins = {}
+    for (const [hexCodePoint, config] of Object.entries(gremlinsConfig)) {
+        let decorationType = {
+            light: lightIcon,
+            dark: darkIcon,
+            overviewRulerColor: config.overviewRulerColor || 'darkred',
+            overviewRulerLane: vscode.OverviewRulerLane.Right
+        }
+
+        if (config.zeroWidth) {
+            decorationType.borderWidth = '1px'
+            decorationType.borderStyle = 'solid'
+            decorationType.borderColor = config.borderColor || 'darkred'
+        } else {
+            decorationType.backgroundColor =
+                config.backgroundColor || 'rgba(255,128,128,.5)'
+        }
+
+        gremlins[charFromHex(hexCodePoint)] = Object.assign({}, config, {
+            hexCodePoint,
+            decorationType: vscode.window.createTextEditorDecorationType(
+                decorationType,
+            )
+        })
     }
 
-    gremlins[charFromHex(hexCodePoint)] = Object.assign({}, config, {
-      hexCodePoint,
-      decorationType: vscode.window.createTextEditorDecorationType(
-        decorationType,
-      ),
-    })
-  }
-
-  return gremlins
+    return gremlins
 }
 
 function charFromHex(hexCodePoint) {
-  return String.fromCodePoint(`0x${hexCodePoint}`)
+    return String.fromCodePoint(`0x${hexCodePoint}`)
 }
 
 function updateDecorations(activeTextEditor, gremlins, regexpWithAllChars) {
-  if (!activeTextEditor) {
-    return
-  }
-
-  const doc = activeTextEditor.document
-
-  const decorationOption = {}
-  for (const char in gremlins) {
-    decorationOption[char] = []
-  }
-
-  for (let lineNum = 0; lineNum < doc.lineCount; lineNum++) {
-    let lineText = doc.lineAt(lineNum)
-    let line = lineText.text
-
-    let match
-    while ((match = regexpWithAllChars.exec(line))) {
-      const matchedCharacter = match[0][0]
-
-      const gremlin = gremlins[matchedCharacter]
-      let startPos = new vscode.Position(lineNum, match.index)
-      let endPos = new vscode.Position(lineNum, match.index + match[0].length)
-      const decoration = {
-        range: new vscode.Range(startPos, endPos),
-        hoverMessage:
-          match[0].length +
-          ' ' +
-          gremlin.description +
-          (match[0].length > 1 ? 's' : '') +
-          ' (unicode U+' +
-          gremlin.hexCodePoint +
-          ') here',
-      }
-
-      decorationOption[matchedCharacter].push(decoration)
+    if (!activeTextEditor) {
+        return
     }
-  }
 
-  for (const [char, gremlin] of Object.entries(gremlins)) {
-    activeTextEditor.setDecorations(
-      gremlin.decorationType,
-      decorationOption[char],
-    )
-  }
+    const doc = activeTextEditor.document
+
+    const decorationOption = {}
+    for (const char in gremlins) {
+        decorationOption[char] = []
+    }
+
+    for (let lineNum = 0; lineNum < doc.lineCount; lineNum++) {
+        let lineText = doc.lineAt(lineNum)
+        let line = lineText.text
+
+        let match
+        while ((match = regexpWithAllChars.exec(line))) {
+            const matchedCharacter = match[0][0]
+
+            const gremlin = gremlins[matchedCharacter]
+            let startPos = new vscode.Position(lineNum, match.index)
+            let endPos = new vscode.Position(lineNum, match.index + match[0].length)
+            const decoration = {
+                range: new vscode.Range(startPos, endPos),
+                hoverMessage:
+                    match[0].length +
+                    ' ' +
+                    gremlin.description +
+                    (match[0].length > 1 ? 's' : '') +
+                    ' (unicode U+' +
+                    gremlin.hexCodePoint +
+                    ') here'
+            }
+
+            decorationOption[matchedCharacter].push(decoration)
+        }
+    }
+
+    for (const [char, gremlin] of Object.entries(gremlins)) {
+        activeTextEditor.setDecorations(
+            gremlin.decorationType,
+            decorationOption[char],
+        )
+    }
 }
 
 function activate(context) {
-  const gremlins = gremlinsFromConfig(context)
+    const gremlins = gremlinsFromConfig(context)
 
-  const regexpWithAllChars = new RegExp(
-    Object.keys(gremlinsConfig)
-      .map(hexCodePoint => charFromHex(hexCodePoint) + '+')
-      .join('|'),
-    'g',
-  )
+    const regexpWithAllChars = new RegExp(
+        Object.keys(gremlinsConfig)
+            .map((hexCodePoint) => charFromHex(hexCodePoint) + '+')
+            .join('|'),
+        'g',
+    )
 
-  vscode.window.onDidChangeActiveTextEditor(
-    editor => updateDecorations(editor, gremlins, regexpWithAllChars),
-    null,
-    context.subscriptions,
-  )
+    vscode.window.onDidChangeActiveTextEditor(
+        (editor) => updateDecorations(editor, gremlins, regexpWithAllChars),
+        null,
+        context.subscriptions,
+    )
 
-  vscode.window.onDidChangeTextEditorSelection(
-    event => updateDecorations(event.textEditor, gremlins, regexpWithAllChars),
-    null,
-    context.subscriptions,
-  )
+    vscode.window.onDidChangeTextEditorSelection(
+        (event) => updateDecorations(event.textEditor, gremlins, regexpWithAllChars),
+        null,
+        context.subscriptions,
+    )
 
-  vscode.workspace.onDidChangeTextDocument(
-    event =>
-      updateDecorations(
+    vscode.workspace.onDidChangeTextDocument(
+        (event) =>
+            updateDecorations(
+                vscode.window.activeTextEditor,
+                gremlins,
+                regexpWithAllChars,
+            ),
+        null,
+        context.subscriptions,
+    )
+
+    updateDecorations(
         vscode.window.activeTextEditor,
         gremlins,
         regexpWithAllChars,
-      ),
-    null,
-    context.subscriptions,
-  )
-
-  updateDecorations(
-    vscode.window.activeTextEditor,
-    gremlins,
-    regexpWithAllChars,
-  )
+    )
 }
 exports.activate = activate
 
 // this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 exports.deactivate = deactivate
