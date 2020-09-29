9cc6da1e2a6b13c40156ffda10d32337de42dfef
Julien Roche <roche.jul@gmail.com>
2020-09-29 21:39:22 +0200

    refactor: have only one Eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,24 @@
 module.exports = {
   extends: ['plugin:prettier/recommended'],
   plugins: ['prettier'],
+  env: {
+    browser: false,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  rules: { 'prettier/prettier': 'error' },
+  rules: {
+    'no-const-assign': 'warn',
+    'no-this-before-super': 'warn',
+    'no-undef': 'warn',
+    'no-unreachable': 'warn',
+    'no-unused-vars': 'warn',
+    'constructor-super': 'warn',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
   settings: {},
 }
